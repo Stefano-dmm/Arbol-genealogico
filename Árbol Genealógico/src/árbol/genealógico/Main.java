3c21609c20cc14c51689951f740946a383e2f50a
Stefano-dmm <122450795+Stefano-dmm@users.noreply.github.com>
2024-11-24 04:18:17 -0400

    Delete Árbol Genealógico directory

--- Árbol Genealógico/src/árbol/genealógico/Main.java
+++
@@ -1,9 +0,0 @@
-package árbol.genealógico;
-
-public class Main {
-    public static void main(String[] args) {
-        // Crear y mostrar la ventana del explorador
-        ExploradorJson explorador = new ExploradorJson();
-        explorador.setVisible(true);
-    }
-}
